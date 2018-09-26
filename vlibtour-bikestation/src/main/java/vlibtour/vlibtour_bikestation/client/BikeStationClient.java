7b2a0dd0fc78d540ac867a4f474f53d2b4e3a384
Hoang Phong NGO <hoangphongngo@macbooks-macbook-pro-2.home>
2018-09-26 23:03:32 +0200

    fixed BikeStationClient

--- vlibtour-bikestation/src/main/java/vlibtour/vlibtour_bikestation/client/BikeStationClient.java
+++ vlibtour-bikestation/src/main/java/vlibtour/vlibtour_bikestation/client/BikeStationClient.java
@@ -1,25 +1,24 @@
 /**
 This file is part of the course CSC5002.
-
 Copyright (C) 2017 Télécom SudParis
-
 This is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This software platform is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
-
 You should have received a copy of the GNU Lesser General Public License
 along with the muDEBS platform. If not, see <http://www.gnu.org/licenses/>.
-
 Initial developer(s): Chantal Taconet
 Contributor(s): Denis Conan
  */
 package vlibtour.vlibtour_bikestation.client;
+
+import vlibtour.vlibtour_bikestation.client.generated_from_json.Station;
+
+//import static org.junit.Assert.assertThat;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -33,47 +32,47 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 
-import vlibtour.vlibtour_bikestation.client.generated_from_json.Station;
-
 /**
- * The bike station REST client example.
+ * The bike station REST client example.
  */
 public final class BikeStationClient {
 	
 	/**
-	 * utility class with no instance.
+	 * the URI.
 	 */
 	private static String restURI;
-	
+
+	/**
+	 * the default constructor is private to avoid instantiation.
+	 */
 	private BikeStationClient() {
 	}
-	
+
+	/**
+	 * the main method.
+	 * 
+	 * @param args
+	 *            there is no command line arguments.
+	 * @throws IOException
+	 *             problem with HTTP connection.
+	 */
 	public static void main(final String[] args) throws IOException {
 		Properties properties = new Properties();
 		FileInputStream input = new FileInputStream("src/main/resources/rest.properties");
 		properties.load(input);
-		restURI = "http://" + properties.getProperty("rest.serveraddress") + "/MyServer";
 		
+		String contract = properties.getProperty("jcdecaux.contract");
+		String apiKey = properties.getProperty("jcdecaux.apiKey");
+								
+		restURI = properties.getProperty("jcdecaux.rooturl") + "/stations/2010?contract=" + contract + "&apiKey=" + apiKey;
+		//https://api.jcdecaux.com/vls/v1/stations/2010?contract=lyon&apiKey=53e15d3779d1c15af0f8a4137a7bbccc2e04f0e0
 		Client client = ClientBuilder.newClient();
 		URI uri = UriBuilder.fromUri(restURI).build();
 		WebTarget service = client.target(uri);
+		System.out.println(uri + "test");
+		System.out.println("the station in json      : \n"
+				+ service.request().accept(MediaType.APPLICATION_JSON).get(String.class));
+
 		
-		System.out.println("init bike station : \n" + 
-				service.path("stations/init").request().accept(MediaType.TEXT_XML).get(String.class));
-		
-		System.out.println("-------------------------------------");
-		
-		System.out.println("all stations in plain text : \n" + 
-				service.path("stations/alltxt").request().accept(MediaType.TEXT_PLAIN).get(String.class));
-		
-		System.out.println("-------------------------------------");
-		
-		System.out.println("all stations in XML		: \n" + 
-				service.path("stations/all").request().accept(MediaType.TEXT_XML).get(Station.class));
-		
-		System.out.println("-------------------------------------");
-		
-		System.out.println("all stations in json    : \n" + 
-				service.path("station/alljson").request().accept(MediaType.APPLICATION_JSON).get(Station.class));
 	}
 }
